711086a991f8430b4d377339459e7bd87fec703f
Ramil-Karipov <omil8@yandex.ru>
2024-02-19 16:32:41 +0500

    Finish

---
+++ src/main/java/controller/StoreController.java
@@ -0,0 +1,31 @@
+package controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class StoreController {
+    private final StoreController storeService;
+
+    public StoreController(StoreController storeService) {
+        this.storeService = storeService;
+    }
+
+    @GetMapping("/add")
+    public void add(@RequestParam("id") List<Long> id) {
+        storeService.add(id);
+
+    }
+
+    @GetMapping("/get")
+    public List<Long> get() {
+        return storeService.get();
+
+    }
+}
---
+++ src/main/java/service/StoreService.java
@@ -0,0 +1,23 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StoreService {
+    private final List<Long> ids;
+
+    public StoreService() {
+        ids = new ArrayList<>();
+    }
+
+    public void add(List<Long> ids) {
+        this.ids.addAll(ids);
+    }
+
+    public List<Long> get() {
+        return ids;
+    }
+}
